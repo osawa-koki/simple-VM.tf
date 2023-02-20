6707713891e3e5eb7ca2ddba16ec4aa687215151
osawa-koki <github@osawa.dev>
2023-02-21 05:31:19 +0900

    いったん書いてみた。

---
+++ main.tf
@@ -0,0 +1,129 @@
+
+variable "project_name" {
+  type        = string
+  default     = "simple-vm-instance"
+  description = "Name of the project."
+}
+
+variable "ssh_public_key_path" {
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+  description = "Path to the public key."
+}
+
+variable "allowed_ip_addresses" {
+  type        = list(string)
+  description = "IP addresses allowed to access the instance."
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.project_name}-rg"
+  location = "japaneast"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.project_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "${var.project_name}-subnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "${var.project_name}-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.project_name}-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "Allow SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = var.allowed_ip_addresses
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow HTTPS"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefixes    = ["*"]
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "${var.project_name}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example"
+    subnet_id                     = azurerm_subnet.example.id
+    public_ip_address_id          = azurerm_public_ip.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    name = var.project_name
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                            = var.project_name
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = azurerm_resource_group.example.location
+  size                            = "Standard_B1ls"
+  admin_username                  = "adminuser"
+  network_interface_ids           = [azurerm_network_interface.example.id]
+  disable_password_authentication = true
+  ssh_key {
+    key_data = file(var.ssh_public_key_path)
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+    version   = "latest"
+  }
+}
