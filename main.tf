e7491db889ce329ac6947a9ea9fce3634607dc82
osawa-koki <github@osawa.dev>
2023-02-21 05:47:24 +0900

    source_address_prefixesの修正。

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "azurerm_network_security_group" "example" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
-    source_address_prefixes    = ["*"]
+    source_address_prefixes    = ["0.0.0.0/0"]
     destination_address_prefix = "*"
   }
 
@@ -82,7 +82,7 @@ resource "azurerm_network_security_group" "example" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
-    source_address_prefixes    = ["*"]
+    source_address_prefixes    = ["0.0.0.0/0"]
     destination_address_prefix = "*"
   }
 }
